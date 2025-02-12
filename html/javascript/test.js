2bfe352aa7617f10acbcb3ac38127ce61d83eafd
Ashish Pandey <ash6i2sh@gmail.com>
2025-02-12 12:19:51 +0530

    up

--- html/javascript/test.js
+++ html/javascript/test.js
@@ -1,4 +1,4 @@
-console.log("this is the log for javascript");
+koconsole.log("this is the log for javascript");
 console.log(1+2);
 console.log("the concatenation of 2 and 3 is " +2 +3);
 
---
+++ javascript/variable.js
@@ -0,0 +1,20 @@
+a=5;
+console.log(a);
+a=10;
+console.log(a);
+
+// var, let , const
+var a=15;
+console.log(a);
+var a=25;
+console.log(a);
+
+let b=30;
+console.log(b);
+    b=35;
+console.log(b);
+
+const c=40;
+console.log(c);
+
+
